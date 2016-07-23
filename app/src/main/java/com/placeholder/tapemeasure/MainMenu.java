a1d26fd72f30fc38a3ad3751bd2511646581c55f
Charles Zhang <czhan10@ocdsb.ca>
2016-07-23 18:23:16 -0400

    Added Main Menu, button_measuringtape redirects to the Measuring Tape activity

--- app/src/main/java/com/placeholder/tapemeasure/MainMenu.java
+++ app/src/main/java/com/placeholder/tapemeasure/MainMenu.java
@@ -1,15 +1,27 @@
 package com.placeholder.tapemeasure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-    }
 
+        // Button Measuring Tape opens Measuring Tape activity
+        Button button_measuringTape = (Button) (findViewById(R.id.button_measuringtape));
+        button_measuringTape.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MeasuringTape.class);
+                startActivity(i);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/placeholder/tapemeasure/MeasuringTape.java
@@ -0,0 +1,13 @@
+package com.placeholder.tapemeasure;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MeasuringTape extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measuring_tape);
+    }
+}
