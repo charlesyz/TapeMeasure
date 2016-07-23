0f3a136fa013b5071e566a417812b640a3ed2aa6
Charles Zhang <czhan10@ocdsb.ca>
2016-07-23 19:21:45 -0400

    Fixed crashing problem with incompatible theme

--- app/src/main/java/com/placeholder/tapemeasure/MainMenu.java
+++ app/src/main/java/com/placeholder/tapemeasure/MainMenu.java
@@ -1,12 +1,12 @@
 package com.placeholder.tapemeasure;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainMenu extends Activity {
+public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/placeholder/tapemeasure/MeasuringTape.java
+++ app/src/main/java/com/placeholder/tapemeasure/MeasuringTape.java
@@ -1,7 +1,10 @@
 package com.placeholder.tapemeasure;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MeasuringTape extends Activity {
 
