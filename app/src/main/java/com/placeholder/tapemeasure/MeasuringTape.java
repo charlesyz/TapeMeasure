a94ee6c726a8029fc115c723fb7e72d821dd9189
Charles Zhang <czhan10@ocdsb.ca>
2016-07-23 21:37:02 -0400

    Added accelerometer framework and display test

--- app/src/main/java/com/placeholder/tapemeasure/MeasuringTape.java
+++ app/src/main/java/com/placeholder/tapemeasure/MeasuringTape.java
@@ -6,11 +6,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MeasuringTape extends Activity {
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.TextView;
+
+public class MeasuringTape extends Activity implements SensorEventListener{
+
+    private TextView xText, yText, zText;
+    private Sensor mySensor;
+    private SensorManager SM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measuring_tape);
+        // Create our Sensor Manager
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        // Accelerometer Sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // Register sensor Listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+        // Assign TextView
+        xText = (TextView)findViewById(R.id.xText);
+        yText = (TextView)findViewById(R.id.yText);
+        zText = (TextView)findViewById(R.id.zText);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        xText.setText("X: " + sensorEvent.values[0]);
+        yText.setText("Y: " + sensorEvent.values[1]);
+        zText.setText("Z: " + sensorEvent.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
     }
 }
