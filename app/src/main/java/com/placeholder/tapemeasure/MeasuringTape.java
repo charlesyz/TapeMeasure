7fb07932abe55ede28833bbb846b9586837ad9e7
Charles Zhang <czhan10@ocdsb.ca>
2016-07-25 21:02:04 -0400

    MeasuringTape - Added velocity calculations and display, added timer

--- app/src/main/java/com/placeholder/tapemeasure/MeasuringTape.java
+++ app/src/main/java/com/placeholder/tapemeasure/MeasuringTape.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
@@ -18,11 +22,15 @@ public class MeasuringTape extends Activity implements SensorEventListener{
     private Sensor mySensor;
     private SensorManager SM;
     private double yAccel = 0.0;
+    private double yAccelOld = 0.0;
     private double velocity = 0.0;
-    private double posDead = 0.03;
-    private double negDead = -0.03;
+    private double posDead = -0.1;
+    private double negDead = -0.2;
     private int timeDiff = 0;
     private boolean isOn = false;
+
+    private Timer timer;
+    private static final String TAG = "timeDiff";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +50,8 @@ public class MeasuringTape extends Activity implements SensorEventListener{
             SENSOR_DELAY_NORMAL 200,000 microseconds(200 milliseconds)
         */
 
+        startTimer();
+
         // Assign TextView
         xText = (TextView)findViewById(R.id.xText);
         yText = (TextView)findViewById(R.id.yText);
@@ -49,6 +59,7 @@ public class MeasuringTape extends Activity implements SensorEventListener{
         velocityText = (TextView)findViewById(R.id.velocityText);
 
         // Assigning on/off buttons
+        // the "ON" button
         Button button_on = (Button) (findViewById(R.id.button_velocity_on));
         button_on.setOnClickListener(new View.OnClickListener() {
 
@@ -57,35 +68,63 @@ public class MeasuringTape extends Activity implements SensorEventListener{
                 isOn = true;
             }
         });
-
+        // "OFF" button
         Button button_off = (Button) (findViewById(R.id.button_velocity_off));
         button_off.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 isOn = false;
+                // Reset text and vars
+                timeDiff = 0;
+                velocity = 0.0;
+                xText.setText("X");
+                yText.setText("Y");
+                zText.setText("Z");
+                velocityText.setText("Velocity");
             }
         });
     }
+
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if (isOn) {
             // set acceleration
+            yAccelOld = yAccel;
             yAccel = sensorEvent.values[1];
 
             // check dead zone and calculate velocity
-            if (yAccel < negDead && yAccel > posDead){
-                velocity += yAccel * 0.02;
+            if (yAccel < negDead || yAccel > posDead){
+                velocity += (yAccelOld + 0.14) * ((double) timeDiff / 1000);
             }
-
             xText.setText("X: " + sensorEvent.values[0]);
             yText.setText("Y: " + sensorEvent.values[1]);
             zText.setText("Z: " + sensorEvent.values[2]);
-            velocityText.setText("Velocity: " + velocity);
+            velocityText.setText("Velocity: " + velocity + "m/s");
+            Log.i(TAG, "TimeDiff = " + timeDiff);
+            // reset
+            timeDiff = 0;
         }
-        else{
-            velocity = 0.0;
+    }
+
+    public void startTimer() {
+
+        myTimerTask counter = new myTimerTask();
+        //set a new Timer
+        timer = new Timer();
+        //schedule the timer, after the first 5000ms the TimerTask will run every 10000ms
+        timer.schedule(counter, 0, 1); //
+    }
+
+    // ever 1 ms checks for change in yAccel and adjust timeDiff
+    class myTimerTask extends TimerTask {
+        public void run() {
+            if (isOn){
+                if (yAccelOld == yAccel){
+                    timeDiff++;
+                }
+            }
         }
     }
 
